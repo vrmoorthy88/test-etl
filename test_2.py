02d9b88e9d79cfd644a8c15df9079ebceed3a157
vrmoorthy88 <35489748+vrmoorthy88@users.noreply.github.com>
2018-01-17 19:19:50 +0000

    Create test_2.py

---
+++ test_2.py
@@ -0,0 +1,30 @@
+# 1. Cloud Auth -
+# gcloud auth application-default login
+# 2. Python g clould bigquery lib
+# pip install --upgrade google-cloud-bigquery
+
+# Run following code
+from google.cloud import bigquery
+
+client = bigquery.Client(project="conductive-fold-191517")
+query_results = client.run_sync_query("SELECT artist_info.name as [artist_name], artist_info.id as [artist_id] FROM [test_dataset.artist] as artist_info LIMIT 100")
+
+# Use standard SQL syntax for queries.
+# See: https://cloud.google.com/bigquery/sql-reference/
+query_results.use_legacy_sql = False
+
+query_results.run()
+
+# Drain the query results by requesting a page at a time.
+page_token = None
+
+while True:
+	rows, total_rows, page_token = query_results.fetch_data(
+		max_results=10,
+		page_token=page_token)
+
+	for row in rows:
+		print(row)
+
+	if not page_token:
+		break
