9a288e2d95d4690ccc1604b8347e4805becacfe4
vrmoorthy88 <35489748+vrmoorthy88@users.noreply.github.com>
2018-01-17 19:49:13 +0000

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -9,3 +9,10 @@ from google.cloud import bigquery
 client = bigquery.Client(project="conductive-fold-191517")
 query = "SELECT 1"
 dataset = client.dataset('test_dataset')
+
+QUERY = ('SELECT 1 as name')
+query_job = client.query(QUERY)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+for row in rows:
+    print(row.name)
