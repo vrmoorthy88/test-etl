d63259f50fb693fe4ddc4177814808754d6001c4
vrmoorthy88 <35489748+vrmoorthy88@users.noreply.github.com>
2018-01-17 19:39:42 +0000

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -7,24 +7,6 @@
 from google.cloud import bigquery
 
 client = bigquery.Client(project="conductive-fold-191517")
-query_results = client.run_sync_query("SELECT 1")
-
-# Use standard SQL syntax for queries.
-# See: https://cloud.google.com/bigquery/sql-reference/
-query_results.use_legacy_sql = False
-
-query_results.run()
-
-# Drain the query results by requesting a page at a time.
-page_token = None
-
-while True:
-	rows, total_rows, page_token = query_results.fetch_data(
-		max_results=10,
-		page_token=page_token)
-
-	for row in rows:
-		print(row)
-
-	if not page_token:
-		break
+query = "SELECT 1"
+dataset = client.dataset('test_dataset')
+job.destination = table
